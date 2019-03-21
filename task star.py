79d40132cb9332bbb3bfefd8bdbbc9ca5e32ca90
SaharAltammemi <41286779+SaharAltammemi@users.noreply.github.com>
2019-03-21 11:17:33 +0300

    Add files via upload

---
+++ task star.py
@@ -0,0 +1,11 @@
+import turtle
+import math
+v=turtle.Turtle()
+v.color('green','yellow')
+v.begin_fill()
+v.speed(20)
+for i in range(80):
+     v.forward(300)
+     v.left(168.5)
+v.end_fill()
+turtle.done()
